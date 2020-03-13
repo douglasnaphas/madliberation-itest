8badfda5e8d56215d5e79367ae951ce7bc278d21
Douglas Naphas <douglasnaphas@gmail.com>
2020-03-13 19:09:56 -0400

    Bump version to 2.0.3

--- App.itest.cjs
+++ App.itest.cjs
@@ -5,7 +5,7 @@ const puppeteer = require("puppeteer");
 const commander = require("commander");
 
 commander
-  .version("1.0.0")
+  .version("2.0.3")
   .option(
     "-s, --site <URL>",
     "Site to run against, default https://passover.lol"
